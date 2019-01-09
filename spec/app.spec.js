9fd652ef2a534eaacf30b56e49909ed6ff9bff13
deltamaze <wcpooley@gmail.com>
2019-01-09 14:39:28 -0600

    updated eslintconfig

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -11,42 +11,42 @@
 // post message has percentage and stock name
 
 
-describe("The Logger", function() {
-  it("can write to a file on the local machine", function() {
-  expect(false).toBe(true);
-});
-});
-
-describe("Yahoo Api", function() {
-  it("returns 3 stocks", function() {
-  expect(false).toBe(true);
-});
+describe('The Logger', () => {
+  it('can write to a file on the local machine', () => {
+    expect(false).toBe(true);
+  });
 });
 
-describe("Slack Messenger", function() {
-  it("can post a message to slack", function() {
-  expect(false).toBe(true);
-});
-});
-
-describe("StockWatch Algorithm", function() {
-  it("Can serialize yahoo stock json", function() {
-  expect(false).toBe(true);
+describe('Yahoo Api', () => {
+  it('returns 3 stocks', () => {
+    expect(false).toBe(true);
+  });
 });
 
-it("Can pull a history of whats been sent before", function() {
-  expect(false).toBe(true);
+describe('Slack Messenger', () => {
+  it('can post a message to slack', () => {
+    expect(false).toBe(true);
+  });
 });
 
-it("Can write to the history file of what was just sent", function() {
-  expect(false).toBe(true);
-});
+describe('StockWatch Algorithm', () => {
+  it('Can serialize yahoo stock json', () => {
+    expect(false).toBe(true);
+  });
 
-it("Wont send alert if symbol within cooldown period", function() {
-  expect(false).toBe(true);
-});
+  it('Can pull a history of whats been sent before', () => {
+    expect(false).toBe(true);
+  });
 
-it("Will send alert when symbol off cooldown period, and update cooldown", function() {
-  expect(false).toBe(true);
-});
+  it('Can write to the history file of what was just sent', () => {
+    expect(false).toBe(true);
+  });
+
+  it('Wont send alert if symbol within cooldown period', () => {
+    expect(false).toBe(true);
+  });
+
+  it('Will send alert when symbol off cooldown period, and update cooldown', () => {
+    expect(false).toBe(true);
+  });
 });
